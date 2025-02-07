3d5fb6e074f11466bf40ce8f65f4dab4a475b0ce
Alex Schofield <git@ajschof.me>
2025-02-07 14:42:49 +0000

    allow standalone execution of discover.py

--- src/discover.py
+++ src/discover.py
@@ -7,3 +7,10 @@ async def discover():
         return [{"name": device.name, "address": device.address} for device in devices]
     except Exception:
         return []
+
+async def main():
+    devices = await discover()
+    print(devices)
+
+if __name__ == "__main__":
+    asyncio.run(main())
