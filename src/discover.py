5a12715276d7166eed0ff776a7b13b0670c8a28c
Alex Schofield <git@ajschof.me>
2025-02-07 15:09:04 +0000

    add debug functionality for viewing full  BT device list

--- src/discover.py
+++ src/discover.py
@@ -1,15 +1,27 @@
 import asyncio
 from bleak import BleakScanner
+import os
 
-async def discover():
+async def discover(debug=False):
     try:
         devices = await BleakScanner.discover()
-        return [{"name": device.name, "address": device.address} for device in devices]
+        if debug:
+            return [{"name": device.name, "address": device.address} for device in devices]
+        else:
+            return [
+                {"name": device.name, "address": device.address}
+                for device in devices
+                if device.name and "TP350S" in device.name
+            ]
     except Exception:
         return []
 
 async def main():
-    devices = await discover()
+    check_debug = os.getenv("DEBUG", "FALSE").upper() == "TRUE"
+    if check_debug:
+        print("DEBUG MODE ENABLED")
+    devices = await discover(debug=check_debug)
+
     print(devices)
 
 if __name__ == "__main__":
--- tests/test_discover.py
+++ tests/test_discover.py
@@ -2,19 +2,36 @@ from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock, patch
 from discover import discover
 from bleak.backends.device import BLEDevice
+import os
 
 class TestDiscover(IsolatedAsyncioTestCase):
     @patch('src.discover.BleakScanner.discover', new_callable=AsyncMock)
     async def test_device_discovery_successful(self, mock_discover):
-        device_1 = BLEDevice(address="AA:BB:CC:DD:EE:FF", name="Device 1", details={}, rssi=-40)
+        device_1 = BLEDevice(address="AA:BB:CC:DD:EE:FF", name="TP350S", details={}, rssi=-40)
         device_2 = BLEDevice(address="GG:HH:II:JJ:KK:LL", name="Device 2", details={}, rssi=-64)
 
-        mock_discover.return_value = [device_1, device_2]
+        mock_discover.return_value = [device_1]
 
         result = await discover()
 
         expected = [
-            {"name": "Device 1", "address": "AA:BB:CC:DD:EE:FF"},
+            {"name": "TP350S", "address": "AA:BB:CC:DD:EE:FF"},
+        ]
+
+        self.assertEqual(result,expected)
+    
+    @patch('src.discover.BleakScanner.discover', new_callable=AsyncMock)
+    async def test_device_discovery_successful_with_debug(self, mock_discover):
+        device_1 = BLEDevice(address="AA:BB:CC:DD:EE:FF", name="TP350S", details={}, rssi=-40)
+        device_2 = BLEDevice(address="GG:HH:II:JJ:KK:LL", name="Device 2", details={}, rssi=-64)
+
+        mock_discover.return_value = [device_1, device_2]
+
+        with patch.dict(os.environ, {"DEBUG": "TRUE"}):
+            result = await discover()
+
+        expected = [
+            {"name": "TP350S", "address": "AA:BB:CC:DD:EE:FF"},
             {"name": "Device 2", "address": "GG:HH:II:JJ:KK:LL"}
         ]
 
