2641baaba1ecdd53aeeedce8b62e7cdf2a953c06
Alex Schofield <git@ajschof.me>
2025-02-07 21:18:50 +0000

    update how device lists are returned and filtered during discovery

--- src/discover.py
+++ src/discover.py
@@ -6,18 +6,16 @@ logger = get_logger("Discovery")
 async def discover(debug=False):
     try:
         logger.info("Starting Bluetooth discovery...")
-        devices = await BleakScanner.discover()
+        scan = await BleakScanner.discover()
         logger.info("Completed Bluetooth discovery!")
-        if debug:
-            filtered_devices = [{"name": device.name, "address": device.address} for device in devices]
-            if not filtered_devices:
-                logger.error("No Bluetooth devices found!")
-        else:
-            filtered_devices = [
-                {"name": device.name, "address": device.address}
-                for device in devices
-                if device.name and "TP350S" in device.name
-            ]
+        logger.debug(scan)
+        devices = [{"name": device.name, "address": device.address} for device in scan]
+        filtered_devices = [
+            device
+            for device in devices
+            if device["name"] and "TP350S" in device["name"]
+        ]
+        return filtered_devices
     except Exception as e:
         logger.error(f"There was an issue during Bluetooth discovery: \n {e}")
         raise
